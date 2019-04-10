30228ee21b83f22c9d20134cb5b08352905831fc
kalai-it19 <46036271+kalai-it19@users.noreply.github.com>
2019-04-10 13:28:01 -0700

    Update and rename 2.insert_customers2_tbl.sql to 2.insert_users_tbl.sql

--- oracle1/2.insert_customers2_tbl.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO customers2 (customer_id , customer_name, city) VALUES (1, 'Tom B. Erichsen', 'Bellevue');
-INSERT INTO customers2 (customer_id , customer_name, city) VALUES (2, 'Kalaiselvi', 'Bothell');
-INSERT INTO customers2 (customer_id , customer_name, city) VALUES (2, 'Test', 'Bothell');
---
+++ oracle1/2.insert_users_tbl.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (user_id , user_name, city) VALUES (1, 'Tom B. Erichsen', 'Bellevue');
+INSERT INTO users (user_id , user_name, city) VALUES (2, 'Kalaiselvi', 'Bothell');
+INSERT INTO users (user_id , user_name, city) VALUES (2, 'Test', 'Bothell');
