e40ce583625afa66a7dd61f4c0df68227a80649d
kalai-it19 <46036271+kalai-it19@users.noreply.github.com>
2019-04-10 10:43:46 -0700

    Update 3.create_customers1_tbl.sql

--- oracle/3.create_customers1_tbl.sql
+++ oracle/3.create_customers1_tbl.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Customers1
 ( user_id number(10) NOT NULL,  
-  user_name varchar2(50) NOT NULL,  
-  city varchar2(50),
-  state1 varchar2(50)   
+  user_name varchar(50) NOT NULL,  
+  city varchar(50),
+  state1 varchar(50)   
 );  
