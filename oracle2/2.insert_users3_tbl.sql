7c233fd3d11ef27f2513078f45a9be6836241258
kalai-it19 <46036271+kalai-it19@users.noreply.github.com>
2019-04-11 12:10:23 -0700

    Update and rename oracle2/2.insert_users3_tbl.sql to oracle3/2.insert_users4_tbl.sql

--- oracle2/2.insert_users3_tbl.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO users3 (user_id , user_name, city) VALUES (1, 'Tom B. Erichsen', 'Bellevue');
---
+++ oracle3/2.insert_users4_tbl.sql
@@ -0,0 +1,1 @@
+INSERT INTO users4 (user_id , user_name, city) VALUES (1, 'Tom B. Erichsen', 'Bellevue');
