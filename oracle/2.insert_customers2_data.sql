e1cb525e5afd465d9b0c5261207a79addeffc229
kalai-it19 <46036271+kalai-it19@users.noreply.github.com>
2019-04-16 16:50:36 -0700

    Update 2.insert_customers2_data.sql

--- oracle/2.insert_customers2_data.sql
+++ oracle/2.insert_customers2_data.sql
@@ -1,1 +1,1 @@
-INSERT INTO Customers2 (user_id , user_name, city, state1) VALUES (1, 'Tom B. Erichsen', 'Bellevue', 'Washington');
+INSERT INTO Customers1 (user_id , user_name, city, state1) VALUES (1, 'Tom B. Erichsen', 'Bellevue', 'Washington');
