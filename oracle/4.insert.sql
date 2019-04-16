baa7100809d6c78483de436463bca067e58f26b0
kalai-it19 <46036271+kalai-it19@users.noreply.github.com>
2019-04-16 16:45:40 -0700

    Update 4.insert.sql

--- oracle/4.insert.sql
+++ oracle/4.insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO Customers3 (user_id , user_name, city, state1) VALUES (2, 'Kalaiselvi1', 'Bothell1', 'Washington');
+INSERT INTO Customers2 (user_id , user_name, city, state1) VALUES (2, 'Kalaiselvi1', 'Bothell1', 'Washington');
