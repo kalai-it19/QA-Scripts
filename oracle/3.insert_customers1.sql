08245d9fe19930588998c58ccec05e0bd3738a81
kalai-it19 <46036271+kalai-it19@users.noreply.github.com>
2019-04-16 17:08:59 -0700

    Create 3.insert_customers1.sql

---
+++ oracle/3.insert_customers1.sql
@@ -0,0 +1,2 @@
+INSERT INTO Customers1 (user_id , user_name, city) VALUES (1, 'Tom B. Erichsen1', 'Bellevue1');
+INSERT INTO Customers1 (user_id , user_name, city) VALUES (2, 'Kalaiselvi1', 'Bothell1');
