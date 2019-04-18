78b4403c166109b3065889014955a7975e3831d5
kalai-it19 <46036271+kalai-it19@users.noreply.github.com>
2019-04-18 13:46:44 -0700

    Update 2.insert.sql

--- mssql/2.insert.sql
+++ mssql/2.insert.sql
@@ -1,15 +1,2 @@
-INSERT INTO [dbo].[Customer]
- ([FirstName]
- ,[LastName]
- ,[PhoneNumber]
- ,[EmailAddress]
- ,[Priority]
- ,[CreateDate])
-VALUES
- ('Jonah'
- ,'Hook'
- ,'777-777-7777'
- ,'jonah@neverdull.com'
- ,1
- ,'2011-09-01')
-GO
+INSERT INTO dbo.Items(ProductID, ProductName)
+ VALUES (1,'keyboard')
