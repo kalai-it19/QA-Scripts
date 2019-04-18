c415fdd586d87b8b2794fae9efa9d0eabf4b14f8
kalai-it19 <46036271+kalai-it19@users.noreply.github.com>
2019-04-18 13:42:45 -0700

    Create 2.insert.sql

---
+++ mssql/2.insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO [dbo].[Customer]
+ ([FirstName]
+ ,[LastName]
+ ,[PhoneNumber]
+ ,[EmailAddress]
+ ,[Priority]
+ ,[CreateDate])
+VALUES
+ ('Jonah'
+ ,'Hook'
+ ,'777-777-7777'
+ ,'jonah@neverdull.com'
+ ,1
+ ,'2011-09-01')
+GO
